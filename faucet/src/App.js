681308a5402bb85fc905da235341cd89a2ed8d97
AmanAli28 <connect.aman.ali@gmail.com>
2022-04-17 20:02:52 +0530

    commit

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -1,9 +1,60 @@
+import React, { useEffect, useState } from "react";
+
+import "./App.css"
+import Web3 from "web3";
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    providerL: null,
+    web3: null
+  })
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      // with meta mask we have access to window.ethereum and to window.web3
+      // meta maks injexts a global API into website
+      // this api allows websites to request users, accounts, read data to blockchain,
+      // sign messages and transactions
+
+      let provider = null;
+      if (window.ethereum) {
+        provider = window.ethereum;
+        try{
+          await provider.enable();
+        } catch {
+          console.log("user denied account access.")
+        }
+      } else if (window.web3){
+        provider = window.web3.currentProvider;
+      } else if (!process.env.production) {
+        provider = new Web3.providers.HttpProvider("http://localhost:7545");
+      }
+    }
+
+    setWeb3Api({
+      web3: new Web3(provider),
+      provider
+    })
+  
+    loadProvider()
+  }, [])
+  
+console.log(web3Api.web3)
+
   return (
-    <div className="App">
-      Hello World!
+    <>
+    <div className="faucet-wrapper">
+      <div className="faucet">
+        <div className="balance-view is-size-2">
+          Current Balance: <strong>10</strong> ETH
+        </div>
+        
+        <button className="btn mr-2">Donate</button>
+        <button className="btn">Withdraw</button>
+      </div>
+
     </div>
+    </>
   );
 }
 
